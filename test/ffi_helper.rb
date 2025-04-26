da0bd0927fee015355fa0b2ca52982f7978ea77d
Andrew Kane <andrew@ankane.org>
2025-04-25 22:34:23 -0700

    Improved test code

--- test/ffi_helper.rb
+++ test/ffi_helper.rb
@@ -1,11 +1,15 @@
 class SQLite3::TestCase
   def before_setup
     skip_tests = [
-      "SQLite3::TestDatabase#test_load_extension_is_defined_on_expected_platforms",
       "SQLite3::TestDiscardDatabase#test_fork_does_not_discard_readonly_connections",
       "SQLite3::TestStatement#test_column_names_are_deduped"
     ]
-    skip_tests << "SQLite3::TestDatabase#test_function_gc_segfault" if stress?
+    if RbConfig::CONFIG["host_os"] =~ /darwin/i
+      skip_tests << "SQLite3::TestDatabase#test_load_extension_is_defined_on_expected_platforms"
+    end
+    if stress?
+      skip_tests << "SQLite3::TestDatabase#test_function_gc_segfault"
+    end
     # TODO report bug
     if RUBY_ENGINE == "truffleruby"
       skip_tests << "IntegrationStatementTestCase#test_long_running_statements_get_interrupted_when_statement_timeout_set"
