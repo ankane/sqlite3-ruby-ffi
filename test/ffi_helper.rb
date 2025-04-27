1c10b40918352c1bc5303ad59fc6fb69feaacd4a
Andrew Kane <andrew@ankane.org>
2025-04-27 01:53:51 -0700

    Updated CI

--- test/ffi_helper.rb
+++ test/ffi_helper.rb
@@ -2,7 +2,7 @@ class SQLite3::TestCase
   def before_setup
     skip_tests = []
     # only fail locally
-    if RbConfig::CONFIG["host_os"] =~ /darwin/i
+    if RbConfig::CONFIG["host_os"] =~ /darwin/i && !ENV["CI"]
       skip_tests << "SQLite3::TestDatabase#test_load_extension_is_defined_on_expected_platforms"
       skip_tests << "SQLite3::TestDiscardDatabase#test_fork_does_not_discard_readonly_connections"
     end
